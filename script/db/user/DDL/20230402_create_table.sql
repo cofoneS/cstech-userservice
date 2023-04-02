2aa177632900183c51aa9f0b5cfbe92e251f9e4f
scofone <salvatore.cofone@jakala.com>
2023-04-02 11:09:48 +0200

    delivery 3: database environment creazione tabelle

---
+++ script/db/user/DDL/20230402_create_table.sql
@@ -0,0 +1,31 @@
+
+drop table IF EXISTS cs_user;
+
+CREATE TABLE cs_user (
+  cs_user_id 		serial4      not null,
+  firstname 		varchar(255) not null,
+  surname			varchar(255) not null,
+  user_key			varchar(255) not null,
+  nickname			varchar(255) null,
+  avatar			varchar(511) null,
+  enabled			bool         not null,
+  created_at        timestamptz  not null,
+  updated_at	    timestamptz  not null,
+  updated_by  		varchar(15)  not null,
+  deleted_at	    timestamptz  null,
+  CONSTRAINT cs_user_pk PRIMARY KEY (cs_user_id)
+);
+
+COMMENT ON COLUMN cs_user.cs_user_id          IS 'Primary key of the table';
+COMMENT ON COLUMN cs_user.firstname 					IS 'Name of the user';
+COMMENT ON COLUMN cs_user.surname             IS 'Surname of the user';
+COMMENT ON COLUMN cs_user.user_key            IS 'Unique key to indefy the user';
+COMMENT ON COLUMN cs_user.nickname            IS 'Nickname value';
+COMMENT ON COLUMN cs_user.avatar              IS 'Avatar image path';
+COMMENT ON COLUMN cs_user.enabled 						IS 'if it is 1 the record is active, otherwise it is disable';
+COMMENT ON COLUMN cs_user.created_at 				IS 'Log time when created record on the table';
+COMMENT ON COLUMN cs_user.updated_at 				IS 'Log time when updated record on the table';
+COMMENT ON COLUMN cs_user.updated_by 				IS 'User that performed the operation';
+COMMENT ON COLUMN cs_user.deleted_at 				IS 'Log time when logical deleted record on the table';
+
+CREATE UNIQUE INDEX cs_user_user_key_UK ON cs_user (user_key);
---
+++ script/db/user_mail/DDL/20230402_create_table.sql
@@ -0,0 +1,29 @@
+
+drop table IF EXISTS cs_user_mail;
+
+CREATE TABLE cs_user_mail (
+  cs_user_mail_id serial4       not null,
+  mail 		      varchar(1023) not null,
+  note        	  text          null,
+  started_at      timestamptz   not null,
+  ended_at	      timestamptz   null,	
+  cs_user_id      int4          not null,
+  enabled		  bool          not null,
+  created_at      timestamptz   not null,
+  updated_at	  timestamptz   not null,
+  updated_by  	  varchar(15)   not null,
+  deleted_at	  timestamptz   null,
+  CONSTRAINT cs_user_mail_pk PRIMARY KEY (cs_user_mail_id)
+);
+
+COMMENT ON COLUMN cs_user_mail.cs_user_mail_id   IS 'Primary key of the table';
+COMMENT ON COLUMN cs_user_mail.mail 			 IS 'Street name value';
+COMMENT ON COLUMN cs_user_mail.started_at        IS 'Start datatime where live in this address';
+COMMENT ON COLUMN cs_user_mail.ended_at          IS 'End datatime where live in this address';
+COMMENT ON COLUMN cs_user_mail.cs_user_id        IS 'FK from cs_user table';
+COMMENT ON COLUMN cs_user_mail.enabled 			 IS 'if it is 1 the record is active, otherwise it is disable';
+COMMENT ON COLUMN cs_user_mail.created_at 		 IS 'Log time when created record on the table';
+COMMENT ON COLUMN cs_user_mail.updated_at 		 IS 'Log time when updated record on the table';
+COMMENT ON COLUMN cs_user_mail.updated_by 		 IS 'User that performed the operation';
+COMMENT ON COLUMN cs_user_mail.deleted_at 		 IS 'Log time when logical deleted record on the table';
+
